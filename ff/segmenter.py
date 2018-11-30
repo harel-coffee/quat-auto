1ff308a3c5db76bdd17756b9dae408d0ceeee2c9
Steve Goering <steve.goering@tu-ilmenau.de>
2018-11-30 16:23:06 +0100

    add fix for missing - and add threads to ffmpeg command to reduce overallcation

--- ff/segmenter.py
+++ ff/segmenter.py
@@ -13,8 +13,8 @@ def create_segments(videofilename, output_folder, segment_time=4, debug=False):
     segment a video
     """
     basename = os.path.basename(videofilename)
-    os.makedirs(output_folder, exists_ok=True)
-    cmd = f"""ffmpeg -y -i {videofilename} -c:v copy -c:a copy -segment_time {segment_time} -f segment {output_folder}/{basename}_%08d.mp4"""
+    os.makedirs(output_folder, exist_ok=True)
+    cmd = f"""ffmpeg -nostdin -loglevel quiet -threads 4 -y -i {videofilename} -c:v copy -c:a copy -segment_time {segment_time} -f segment {output_folder}/{basename}_%08d.mp4 2>/dev/null"""
     if debug:
         print(cmd)
     shell_call(cmd)

9d587c3d1d6cb56cfbd94696eb9f4402265c1011
Steve Goering <steve.goering@tu-ilmenau.de>
2018-11-30 15:47:43 +0100

    add segmenter

---
+++ ff/segmenter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import shutil
+import os
+import glob
+
+from ..utils.system import shell_call
+from ..utils.system import lglob
+
+
+
+def create_segments(videofilename, output_folder, segment_time=4, debug=False):
+    """
+    segment a video
+    """
+    basename = os.path.basename(videofilename)
+    os.makedirs(output_folder, exists_ok=True)
+    cmd = f"""ffmpeg -y -i {videofilename} -c:v copy -c:a copy -segment_time {segment_time} -f segment {output_folder}/{basename}_%08d.mp4"""
+    if debug:
+        print(cmd)
+    shell_call(cmd)
+    return lglob(f"{output_folder}/{basename}_*.mp4")

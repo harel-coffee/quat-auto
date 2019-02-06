92bbdb52b62f624370bafdebf19b03a787bd3a2d
Steve <stg7@gmx.de>
2019-02-06 13:55:35 +0100

    add licence everythere

--- tools/do_parallel_by_file.py
+++ tools/do_parallel_by_file.py
@@ -5,20 +5,19 @@
     example usage:
         ./do_parallel.py *.gz --script ./whatever.sh -s
 
-    This file is part of PhrasIt.
-
-    PhrasIt is free software: you can redistribute it and/or modify
+    This file is part of quat.
+    quat is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
-
-    PhrasIt is distributed in the hope that it will be useful,
+    quat is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
-
     You should have received a copy of the GNU General Public License
-    along with PhrasIt.  If not, see <http://www.gnu.org/licenses/>.
+    along with quat. If not, see <http://www.gnu.org/licenses/>.
+
+    Author: Steve Göring
 """
 
 import re
--- utils/fileutils.py
+++ utils/fileutils.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python3
 """
-    Copyright 2016-today
+    This file is part of quat.
+    quat is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    quat is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with quat. If not, see <http://www.gnu.org/licenses/>.
+
+    Author: Steve Göring
+"""
+"""
     Some collected and hopefully usefule methods for file handling
-
-    Author: Steve Göring, Konstantin Brand
-
+    Author: Steve Göring
 """
 
 import os

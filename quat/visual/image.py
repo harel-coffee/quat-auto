91d2279a7a618ca8f8942e745e5c846c63fc6549
Steve <stg7@gmx.de>
2020-02-04 15:30:08 +0100

    change return types of some features

--- quat/visual/image.py
+++ quat/visual/image.py
@@ -234,7 +234,7 @@ def calc_brisque_features(image, gray=False):
     if not gray:
         image = skimage.color.rgb2gray(image)
     brisque_features = skvideo.measure.brisque_features(image)
-    return brisque_features.tolist()[0]
+    return [float(x) for x in brisque_features.tolist()[0]]
 
 
 def calc_niqe_features(image, gray=False):
@@ -302,7 +302,8 @@ def ceiq(image, gray=False):
     add4 = hd.mean()
     add5 = hd.std()
 
-    return [f1, f2, f3, f4, f5, add1, add2, add3, add4, add5]
+    res = [f1, f2, f3, f4, f5, add1, add2, add3, add4, add5]
+    return [float(x) for x in res]
 
 
 def calc_noise(frame):
@@ -314,4 +315,4 @@ def calc_noise(frame):
     -------
     mean value of all channels for std value of noise assuming that the noise has a Gaussian distribution
     """
-    return skimage.restoration.estimate_sigma(frame, average_sigmas=True, multichannel=True)
+    return float(skimage.restoration.estimate_sigma(frame, average_sigmas=True, multichannel=True))

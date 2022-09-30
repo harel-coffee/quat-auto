ca48db6df89636ca553019c3b554e9cdd6804728
Steve <stg7@gmx.de>
2022-09-30 14:51:32 +0200

    update psnr calculation

--- quat/tools/psnr.py
+++ quat/tools/psnr.py
@@ -37,112 +37,66 @@ import skvideo.io
 import skimage.transform
 import skimage.io
 
+from quat.log import *
+from quat.video import iterate_by_frame_two_videos
 
-def file_open(filename, mode="r"):
-    """ Open a file, and if you add bz2 to filename a compressed file will be opened
-    """
-    if "bz2" in filename:
-        return bz2.open(filename, mode + "t")
-    if "gz" in filename:
-        return gzip.open(filename, mode + "t")
-    return open(filename, mode)
-
-
-def iterate_by_frame(video_filename):
-    """ iterator over all frames a video_filename """
-    for frame in skvideo.io.vreader(video_filename):
-        yield frame
-    raise StopIteration
-
-
-def colorgreen(m):
-    return "\033[92m" + m + "\033[0m"
-
-
-def lInfo(msg):
-    print(colorgreen("[INFO ] ") + str(msg), flush=True)
-
-
-def read_videos_frame_by_frame(
-    distortedvideo,
-    referencevideo,
-    per_frame_function,
-    per_frame_function_additional_params={},
-    debug=False,
-):
-    distortedvideo_it = iterate_by_frame(distortedvideo)
-    referencevideo_it = iterate_by_frame(referencevideo)
-    k = 0
-    results = []
-    last_x = None
-    last_y = None
-    try:
-        while True:
-            lInfo("frame: {}".format(k))
-            start_time = time.time()
-
-            x = next(referencevideo_it)
-            y = next(distortedvideo_it)
-            results.append(
-                per_frame_function(
-                    x, y, last_x, last_y, **per_frame_function_additional_params
-                )
-            )
-            k += 1
-            last_x = x
-            last_y = y
-            if k == 50 and debug:
-                break
-            time_per_frame = time.time() - start_time
-            lInfo("time per frame: {}".format(time_per_frame))
-
-    except StopIteration:
-        lInfo("reached end")
-    return results
-
-
-def calc_psnr(x, y, last_x, last_y, bitdepth=10):
-    new_shape = (
-        max(x.shape[0], y.shape[0]),
-        max(x.shape[1], y.shape[1]),
-        max(x.shape[2], y.shape[2]),
-    )
-
-    if new_shape != x.shape:
-        x = skimage.transform.resize(x, new_shape[0:2], mode="reflect")
-    if new_shape != y.shape:
-        y = skimage.transform.resize(y, new_shape[0:2], mode="reflect")
-
-    maxvalue = np.int(2 ** bitdepth - 1)
-    maxsq = maxvalue ** 2
-
-    mse = np.mean((x - y) ** 2)
-    psnr = 10 * np.log10(maxsq / mse)
-    lInfo("psnr: {}".format(psnr))
-    return psnr
+from quat.utils.fileutils import file_open
+from quat.visual.fullref import PSNR
+from quat.utils.fileutils import get_filename_without_extension
+from quat.utils.fileutils import write_json
+from quat.video import advanced_pooling
 
 
 def psnr_report(video, reference, output_dir):
-    reportname = (
-        output_dir
-        + "/"
-        + os.path.splitext(os.path.basename(video))[0]
-        + "_psnr.json.bz2"
+    reportname = os.path.join(
+            output_dir,
+            os.path.splitext(os.path.basename(video))[0] + "_psnr.json.bz2"
     )
-    results = read_videos_frame_by_frame(video, reference, calc_psnr)
-    values = {}
-    values["per_frame"] = results
-    r = np.array(results)
-    values["mean"] = r.mean()
-    values["median"] = np.median(r)
-    values["max"] = r.max()
-    values["min"] = r.min()
-    values["std"] = r.std()
+
+    features = {
+        "psnr": PSNR(),
+    }
+
+    results = []
+    frame_number = 1
+    for dis, ref in iterate_by_frame_two_videos(video, reference, convert=True):
+        # upscale frames to same size, if needed
+        new_shape = (
+            max(dis.shape[0], ref.shape[0]),
+            max(dis.shape[1], ref.shape[1]),
+            max(dis.shape[2], ref.shape[2]),
+        )
+
+        if new_shape != dis.shape:
+            dis = skimage.transform.resize(dis, new_shape[0:2], mode="reflect")
+        if new_shape != ref.shape:
+            ref = skimage.transform.resize(ref, new_shape[0:2], mode="reflect")
+
+        # calculate scores
+        r = {}
+        for f in features:
+            v = features[f].calc_ref_dis(dis, ref)
+            r[f] = v
+
+        r["frame"] = frame_number
+        results.append(r)
+        jprint(r)
+        frame_number += 1
+
+    pooled = {}
+    for f in features:
+        pooled = dict(pooled, **advanced_pooling(features[f].get_values(), f))
+
+    res = {
+        "video": video,
+        "values": results,
+        "pooled": pooled
+    }
 
     with file_open(reportname, "w") as rep:
-        json.dump(values, rep)
+        json.dump(res, rep)
 
-    return {"video": video, "psnr_mean": values["mean"]}
+    return res
 
 
 def main(_=[]):
